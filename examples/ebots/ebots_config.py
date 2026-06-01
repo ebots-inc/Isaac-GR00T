347e4f41a9725c44ec1bf710e23b43bc15b96e24
gayatrid-ebots <gdavuluri@ebots.com>
2026-06-01 20:15:00 +0000

    Add Ebots EEF XYZ_ROTVEC modality config as per latest GR00T N1.7 model

--- examples/ebots/ebots_config.py
+++ examples/ebots/ebots_config.py
@@ -72,7 +72,7 @@ ebots_config = {
 ebots_left_arm_config = {
     "video": ModalityConfig(
         delta_indices=[0],
-        modality_keys=["cam_high", "cam_left_wrist"],
+        modality_keys=["cam_high", "cam_left_wrist", "cam_side"],
     ),
     "state": ModalityConfig(
         delta_indices=[0],
@@ -92,6 +92,43 @@ ebots_left_arm_config = {
                 rep=ActionRepresentation.RELATIVE,
                 type=ActionType.NON_EEF,
                 format=ActionFormat.DEFAULT,
+            ),
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF,
+                format=ActionFormat.DEFAULT,
+            ),
+        ],
+    ),
+    "language": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["annotation.human.task_description"],
+    ),
+}
+
+ebots_left_arm_eef_rotvec_config = {
+    "video": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["cam_high", "cam_left_wrist", "cam_side"],
+    ),
+    "state": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=[
+            "arm_l",
+            "arm_l_finger",
+        ],
+    ),
+    "action": ModalityConfig(
+        delta_indices=list(range(0, 16)),
+        modality_keys=[
+            "arm_l",
+            "arm_l_finger",
+        ],
+        action_configs=[
+            ActionConfig(
+                rep=ActionRepresentation.RELATIVE,
+                type=ActionType.EEF,
+                format=ActionFormat.XYZ_ROTVEC,
             ),
             ActionConfig(
                 rep=ActionRepresentation.ABSOLUTE,
@@ -147,6 +184,7 @@ ebots_right_arm_config = {
 _VARIANT_TO_CONFIG = {
     "bimanual": ebots_config,
     "left_arm": ebots_left_arm_config,
+    "left_arm_eef_rotvec": ebots_left_arm_eef_rotvec_config,
     "right_arm": ebots_right_arm_config,
 }
 
